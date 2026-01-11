f5cf41054e57345a900ff05cc1322f69553075c6
Gabriel Telles <telleswq@gmail.com>
2026-01-10 21:01:21 -0300

    refactor: add hooks to queries and mutations

--- components/common/cart.tsx
+++ components/common/cart.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
 import { ShoppingBasketIcon } from "lucide-react";
-import Image from "next/image";
 
-import { getCart } from "@/actions/get-cart";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { formatCentsToBRL } from "@/helpers/money";
+import { useCart } from "@/hooks/queries/use-cart";
 
+import { Badge } from "../ui/badge";
 import { ScrollArea } from "../ui/scroll-area";
 import { Separator } from "../ui/separator";
 import {
@@ -21,10 +19,7 @@ import {
 import CartItem from "./cart-item";
 
 export const Cart = () => {
-  const { data: cart, isPending: cartIsLoading } = useQuery({
-    queryKey: ["cart"],
-    queryFn: () => getCart(),
-  });
+  const { data: cart } = useCart();
 
   // Calcula a quantidade total de itens no carrinho
   const totalItems =
